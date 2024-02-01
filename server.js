38316e35390c970f0b50412eb8bf07de4b1e7243
Santosh Jena <jenasantosh50@gmail.com>
2024-02-01 23:14:25 +0530

    Added Cart Server Support using ExpressJS

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+const PORT = 3000;
+const app = express();
+
+const userCart ={}; // This will temporarily store prodId:count till server restarts/crashes
+
+app.use(bodyParser.json());
+
+app.get("/cart", (req, res) => {
+  res.send(userCart)
+});
+
+app.post("/cart", (req, res) => {
+  var pid = req.body.productID;
+  var typ = req.body.updateType;
+  if(userCart[pid]){
+    if(typ == "add"){
+        userCart[pid]+=1;
+    }
+    else if(typ == "remove"){
+        userCart[pid]-=1;
+    }
+  }
+  else{
+    userCart[pid]=1;
+  }
+
+  res.send("ID: "+pid+" updated with count: "+userCart[pid]);
+});
+
+app.listen(PORT,()=>{
+    console.log("Server Running on PORT:"+PORT);
+})
