d3385be46be5535455e1baa7f14394cb2590f96d
Santosh Jena <jenasantosh50@gmail.com>
2024-02-06 21:24:23 +0530

    fixed edge cases on cart counts

--- cart.js
+++ cart.js
@@ -14,18 +14,27 @@ app.get("/cart", (req, res) => {
 app.post("/cart", (req, res) => {
   var pid = req.body.productID;
   var typ = req.body.updateType;
+  // var user = req.body.tempUserSession; //to be implemented
   if(userCart[pid]){
     if(typ == "add"){
         userCart[pid]+=1;
     }
+    else if(typ == "remove" && userCart[pid]==1){
+        delete userCart[pid];
+        res.send("ID: "+pid+" removed from the cart!");
+    }
     else if(typ == "remove"){
-        userCart[pid]-=1;
+      userCart[pid]-=1;
     }
   }
   else{
-    userCart[pid]=1;
+    if(typ=="add"){
+      userCart[pid]=1;
+    }
+    else{
+      res.send("Cannot remove product as it is unavailable in the cart!");
+    }
   }
-
   res.send("ID: "+pid+" updated with count: "+userCart[pid]);
 });
 
