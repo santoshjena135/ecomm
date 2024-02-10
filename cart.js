29ac619d7842b4ec7f5bc23e356131e86cc5cbac
Santosh Jena <jenasantosh50@gmail.com>
2024-02-10 18:06:02 +0530

    optimized cart.js

--- cart.js
+++ cart.js
@@ -9,20 +9,24 @@ app.use(bodyParser.json());
 
 app.get("/cart/:user", (req, res) => {
   const user = req.params.user;
-  if(user == "all") // fetches all user carts, only for testing purpose
+  if(user === "all") // fetches all user carts, only for testing purpose
   {
-    res.send(userCarts);
+    return res.send(userCarts);
   }
   else{
     const userCart = userCarts[user];
-    res.send(userCart)
+    res.send(userCart);
   }
 });
 
 app.post("/cart", (req, res) => {
-  var pid = req.body.productID;
-  var typ = req.body.updateType;
+  var productId = req.body.productID;
+  var updateType = req.body.updateType;
   var user = req.body.tempUserSession; 
+
+  if (!productId || !updateType || !user) {
+    return res.status(400).send("Missing required parameters");
+  }
 
   if(!userCarts[user])
   {
@@ -30,27 +34,27 @@ app.post("/cart", (req, res) => {
   }
   const userCart = userCarts[user];
 
-  if(userCart[pid]){
-    if(typ == "add"){
-        userCart[pid]+=1;
+  if(userCart[productId]){
+    if(updateType === "add"){
+        userCart[productId]+=1;
     }
-    else if(typ == "remove" && userCart[pid]==1){
-        delete userCart[pid];
-        res.send("ID: "+pid+" removed from the cart for the user: "+user);
+    else if(updateType === "remove" && userCart[productId]==1){
+        delete userCart[productId];
+        return res.send("ID: "+productId+" removed from the cart for the user: "+user);
     }
-    else if(typ == "remove"){
-      userCart[pid]-=1;
+    else if(updateType === "remove"){
+      userCart[productId]-=1;
     }
   }
   else{
-    if(typ=="add"){
-      userCart[pid]=1;
+    if(updateType === "add"){
+      userCart[productId]=1;
     }
     else{
-      res.send("Cannot remove product as it is unavailable in the cart!");
+      return res.status(400).send("Cannot remove product as it is unavailable in the cart!");
     }
   }
-  res.send("ID: "+pid+" updated with count: "+userCart[pid]+" for user: "+user);
+  res.send("ID: "+productId+" updated with count: "+userCart[productId]+" for user: "+user);
 });
 
 app.listen(PORT,()=>{
